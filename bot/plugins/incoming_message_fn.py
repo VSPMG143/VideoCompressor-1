327f411d39c60c7dfa7844fbb382f8896cddbe30
Dayam Zaidi <69107551+mrdayamzaidi@users.noreply.github.com>
2021-01-05 14:25:41 +0530

    Update incoming_message_fn.py

--- bot/plugins/incoming_message_fn.py
+++ bot/plugins/incoming_message_fn.py
@@ -39,9 +39,21 @@ from bot.helper_funcs.utils import(
 async def incoming_start_message_f(bot, update):
     """/start command"""
     # LOGGER.info(update)
-    await bot.send_message(
+     await bot.send_message(
         chat_id=update.chat.id,
-        text=Localisation.START_TEXT,
+        text=Translation.START_TEXT.format(update.from_user.first_name),
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton('Support Channel', url='https://t.me/whiteeyebots'),
+                    InlineKeyboardButton('Support Group', url='https://t.me/whiteeyebotschat')
+                ],
+                [
+                    InlineKeyboardButton('WgiteEyeBot-List', url='https://t.me/WhiteEyeBots/5'),
+                    InlineKeyboardButton('Devs', url='https://t.me/WhiteEyeDevs')
+                ]
+            ]
+        ),
         reply_to_message_id=update.message_id
     )
     
