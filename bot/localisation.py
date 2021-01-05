e9e1041561600c6377cbd2da74229254404c70ce
Dayam Zaidi <69107551+mrdayamzaidi@users.noreply.github.com>
2021-01-05 14:57:47 +0530

    Update localisation.py

--- bot/localisation.py
+++ bot/localisation.py
@@ -21,52 +21,52 @@ class Localisation:
      START_TEXT = """ <b> Hi {} , I'm A WhiteEye-CompressorBot I Can Compress Your Videos Within Minutes Just Send Me A Video And See The Magic.</b> \n
 <b>Bot Maintained By: [WhiteEyeBots](https://t.me/whiteeyebots)</b> \n
 <b>Do /help for more...</b> \n
-"""
-   
-    ABS_TEXT = " Please don't be selfish."
+"""  
+     
+     ABS_TEXT = "Please don't be selfish."
     
-    FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
+     FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
     
     
-    DOWNLOAD_START = "📥 Downloading 📥 \n"
+     DOWNLOAD_START = "📥 Downloading 📥 \n"
     
-    UPLOAD_START = "📤 Uploading 📤 \n"
+     UPLOAD_START = "📤 Uploading 📤 \n"
     
-    COMPRESS_START = "📀 Trying to compress... 📀"
+     COMPRESS_START = "📀 Trying to compress... 📀"
     
-    RCHD_BOT_API_LIMIT = "size greater than maximum allowed size (50MB). Neverthless, trying to upload."
+     RCHD_BOT_API_LIMIT = "size greater than maximum allowed size (50MB). Neverthless, trying to upload."
     
-    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.5GB due to Telegram API limitations."
+     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.5GB due to Telegram API limitations."
     
-    COMPRESS_SUCCESS = "📥 Downloaded in {}\n\n📀 Compressed in {}\n\n📤 Uploaded in {}"
+     COMPRESS_SUCCESS = "📥 Downloaded in {}\n\n📀 Compressed in {}\n\n📤 Uploaded in {}"
 
-    COMPRESS_PROGRESS = "⏳ ETA: {}\n🚀 Progress: {}%"
+     COMPRESS_PROGRESS = "⏳ ETA: {}\n🚀 Progress: {}%"
 
-    SAVED_CUSTOM_THUMB_NAIL = "Custom video / file thumbnail saved. This image will be used in the video / file."
+     SAVED_CUSTOM_THUMB_NAIL = "Custom video / file thumbnail saved. This image will be used in the video / file."
     
-    DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail cleared succesfully."
+     DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail cleared succesfully."
     
-    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "✅ Media cleared succesfully."
+     FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "✅ Media cleared succesfully."
     
-    SAVED_RECVD_DOC_FILE = "✅ Downloaded Successfully."
+     SAVED_RECVD_DOC_FILE = "✅ Downloaded Successfully."
     
-    CUSTOM_CAPTION_UL_FILE = " "
+     CUSTOM_CAPTION_UL_FILE = " "
     
-    NO_CUSTOM_THUMB_NAIL_FOUND = "No Custom ThumbNail found."
+     NO_CUSTOM_THUMB_NAIL_FOUND = "No Custom ThumbNail found."
     
-    NO_VOID_FORMAT_FOUND = "no-one gonna help you\n{}"
+     NO_VOID_FORMAT_FOUND = "no-one gonna help you\n{}"
     
-    USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
+     USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
     
-    FF_MPEG_RO_BOT_STOR_AGE_ALREADY_EXISTS = "⚠️ Already One Process going on. \n or \n A media already exists. \n  Please send /cancel to delete existing media. ⚠️"
+     FF_MPEG_RO_BOT_STOR_AGE_ALREADY_EXISTS = "⚠️ Already One Process going on. \n or \n A media already exists. \n  Please send /cancel to delete existing media. ⚠️"
     
-    HELP_MESSAGE = get_config(
-        "STRINGS_HELP_MESSAGE",
-        "Hi am Video Compressor Bot \n\n1. Sent your telegram big video file \n2. Reply the file - /compress And Persentage \nEg:- <code>/compress 50</code> \n\nSupport Group :@WhiteEyeBots"
-    )
-    WRONG_MESSAGE = get_config(
-        "STRINGS_WRONG_MESSAGE",
-        "current CHAT ID: <code>{CHAT_ID}</code>"
-    )
+     HELP_MESSAGE = get_config(
+         "STRINGS_HELP_MESSAGE",
+         "Hi am Video Compressor Bot \n\n1. Sent your telegram big video file \n2. Reply the file - /compress And Persentage \nEg:- <code>/compress 50</code> \n\nSupport Group :@WhiteEyeBots"
+     )
+     WRONG_MESSAGE = get_config(
+         "STRINGS_WRONG_MESSAGE",
+         "current CHAT ID: <code>{CHAT_ID}</code>"
+     )
 
     
