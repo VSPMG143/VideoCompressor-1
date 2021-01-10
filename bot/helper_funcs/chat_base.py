5a0ffc90a288d09edc6900c9411eb781ad1f370e
Dayam Zaidi <69107551+mrdayamzaidi@users.noreply.github.com>
2021-01-10 10:44:10 +0530

    Update chat_base.py

--- bot/helper_funcs/chat_base.py
+++ bot/helper_funcs/chat_base.py
@@ -18,7 +18,7 @@ else:
     from config import Config
 
 # the Strings used for this "thing"
-from localisation import localisation
+from localisation import Localisation
 
 from pyrogram import Client, Filters
 
