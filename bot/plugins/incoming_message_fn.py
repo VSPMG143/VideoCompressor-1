1b6dc461196dfd8ef6d911d0e2613c28b974f676
Dayam Zaidi <69107551+mrdayamzaidi@users.noreply.github.com>
2021-01-10 11:12:51 +0530

    Update incoming_message_fn.py

--- bot/plugins/incoming_message_fn.py
+++ bot/plugins/incoming_message_fn.py
@@ -59,9 +59,9 @@ async def incoming_start_message_f(bot, update):
     
 async def incoming_compress_message_f(bot, update):
   """/compress command"""
-   if update.from_user.id in Config.BANNED_USERS:
-        await update.reply_text("You are B A N N E D")
-        return
+    if update.from_user.id in Config.BANNED_USERS:
+       await update.reply_text("You are B A N N E D")
+       return
     TRChatBase(update.from_user.id, update.text, "/compress")
     update_channel = Config.UPDATE_CHANNEL
     if update_channel:
